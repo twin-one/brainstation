f482c02187fd6229040ffc4b2c4dfcf6cfdbedc5
Charley Ballantyne <charley@twin-one.com>
2017-11-21 15:00:54 -0800

    Added butoon to get from API

--- weather-visualizer/src/App.js
+++ weather-visualizer/src/App.js
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      dateStart: '2017-01-01',
+      dateEnd: '2017-11-01'
+    }
+  }
+  
+  getData = () => {
+    let url = 'https://api.coindesk.com/v1/bpi/historical/close.json?start=' + this.state.dateStart + '&end=' + this.state.dateEnd
+    axios.get(url)
+      .then(response => this.setState({
+          bitcoinData: response.data
+        })
+      )
+    .then (console.log(this.state))
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,8 +30,9 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+         Get bitcoin data
         </p>
+        <button onClick={this.getData}>Get Bitcoin Data</button>
       </div>
     );
   }
