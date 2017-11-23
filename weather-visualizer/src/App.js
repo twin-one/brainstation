7807f85730b141333bca33aaec7a2c2f074aef7f
Sandro Nasso <sandro.ordnas@live.ca>
2017-11-23 14:05:14 -0500

    Chart imported into React App

--- weather-visualizer/src/App.js
+++ weather-visualizer/src/App.js
@@ -3,6 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 
+/* C3 - react-c3js */
+import C3Chart from 'react-c3js';
+import 'c3/c3.css';
+
+
 class App extends Component {
   constructor() {
     super()
@@ -23,6 +28,13 @@ class App extends Component {
   }
 
   render() {
+    const data = {
+      columns: [
+        ['data1', 30, 200, 100, 400, 150, 250],
+        ['data2', 50, 20, 10, 40, 15, 25]
+      ]
+    };
+
     return (
       <div className="App">
         <header className="App-header">
@@ -31,6 +43,8 @@ class App extends Component {
         </header>
         <p className="App-intro">
          Get bitcoin data
+         <br/>
+         <C3Chart data={data} />
         </p>
         <button onClick={this.getData}>Get Bitcoin Data</button>
       </div>
