5f30625a918cc72baf5117ed52d9515362565880
Charley Ballantyne <charley@twin-one.com>
2017-11-23 12:05:19 -0800

    API is connected to chart and renders data

--- weather-visualizer/src/App.js
+++ weather-visualizer/src/App.js
@@ -12,28 +12,40 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      dateStart: '2017-01-01',
-      dateEnd: '2017-11-01'
+      dateStart: '2017-10-01',
+      dateEnd: '2017-10-07',
+      data: {
+        columns: [
+          []
+        ]
+      }
     }
   }
   
   getData = () => {
     let url = 'https://api.coindesk.com/v1/bpi/historical/close.json?start=' + this.state.dateStart + '&end=' + this.state.dateEnd
+    let data = []
     axios.get(url)
-      .then(response => this.setState({
-          bitcoinData: response.data
+      .then(response => {
+      data = Object.values(response.data.bpi)
+      this.setState({
+          data: {
+            columns: [
+              ['data1', ...data]
+            ]
+          }
         })
-      )
-    .then(console.log(this.state))
+      })
+  }
+
+  componentDidMount() {
+    this.getData ()
+    
   }
 
   render() {
-    const data = {
-      columns: [
-        ['data1', 30, 200, 100, 400, 150, 250],
-        ['data2', 50, 20, 10, 40, 15, 25]
-      ]
-    };
+    let bitData = this.state.bitcoinData
+    console.log(this.state)
 
     return (
       <div className="App">
@@ -43,9 +55,8 @@ class App extends Component {
         </header>
         <p className="App-intro">
          Get bitcoin data
-         <br/>
-         <C3Chart data={data} />
         </p>
+        <C3Chart data={this.state.data} />
         <button onClick={this.getData}>Get Bitcoin Data</button>
       </div>
     );
